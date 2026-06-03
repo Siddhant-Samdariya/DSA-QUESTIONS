3f94522e8d1ed51a132c16a3c76382ada8e5746e
Siddhant-Samdariya <samdariyasiddhant86@gmail.com>
2026-06-03 19:08:17 +0530

    Medium it was not a coding problem i think

---
+++ Leetcode/Med/1227. Airplane Seat Assignment Probability.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        if(n==1) return double(1);
+        else return 1.0/2;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
