9ffbeece0e6136aac0c6237412a44cb8c6efee68
Siddhant-Samdariya <samdariyasiddhant86@gmail.com>
2026-06-08 20:34:34 +0530

    Medium Done

---
+++ Leetcode/Med/2161. Partition Array According to Given Pivot.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> p;
+        for(int i : nums)
+        {
+            if(i<pivot) .push_back(i);
+        }
+        for(int i : nums)
+        {
+            if(i==pivot) p.push_back(i);
+        }
+        for(int i : nums)
+        {
+            if(i>pivot) p.push_back(i);
+        }
+        return p;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
