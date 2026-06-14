39bf66f8fdd0ed6d09f2e78902ab4599c26cd33b
Siddhant-Samdariya <samdariyasiddhant86@gmail.com>
2026-06-14 22:38:18 +0530

    Done

--- Leetcode/Med/3867. Sum of GCD of Formed Pairs.cpp
+++ Leetcode/Med/3867. Sum of GCD of Formed Pairs.cpp
@@ -8,13 +8,13 @@ public:
         int mx = 0;
         for (int i = 0; i < nums.size(); i++) {
             mx = max(mx, nums[i]);
-            g.push_back(gcd(nums[i], mx));
+            // g.push_back(gcd(nums[i], mx));
         }
         sort(g.begin(), g.end());
         int l = 0, r = g.size() - 1;
         long long int sum = 0;
         while (l < r) {
-            sum += gcd(g[l], g[r]);
+            // sum += gcd(g[l], g[r]);
             l++;
             r--;
         }
