a36db5a65984b024a476438b25cb850ee0ebd1c9
Siddhant-Samdariya <samdariyasiddhant86@gmail.com>
2026-06-26 18:02:43 +0530

    Medium Done

---
+++ Leetcode/Med/462. Minimum Moves to Equal Array Elements II.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        long long int moves=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            moves+=abs(nums[i]-nums[nums.size()/2]);
+        }
+        return moves;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
